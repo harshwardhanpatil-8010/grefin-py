e80d70c85fae0d46ace33d492147c4ce91c31e0a
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-01-24 00:28:27 +0530

    new

--- python/app/main.py
+++ python/app/main.py
@@ -11,11 +11,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],  # Change this to specific origins for production
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all HTTP methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 logging.basicConfig(level=logging.INFO)
 
 # Load pre-trained model and preprocessors
