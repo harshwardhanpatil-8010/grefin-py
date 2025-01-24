a677bfaafa0a49ac4898967551e2d98361babfbb
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-01-24 17:11:08 +0530

    new

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import uvicorn 
+PORT = int(os.get('PORT', 8000))
+HOST = '0.0.0.0'
+
+if __name__ == '__main__':
+    uvicorn.run('app.api:app', host=HOST, port=PORT, reload = True)
--- python/gunicorn_config.py
+++
@@ -1,2 +0,0 @@
-bind = "0.0.0.0:8000"
-workers = 2  # Adjust this based on your server's CPU cores
